07a1042258967912dcbc6d29e84f7a6b5276340c
spiderq2697 <spiderq10@gmail.com>
2022-04-12 13:26:25 +0900

    feat: 회원 수정, 삭제, 비밀번호 변경 기능을 추가한다

--- 0412/CRUD_TEMPLATE_v1.3/accounts/views.py
+++ 0412/CRUD_TEMPLATE_v1.3/accounts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login
-from django.contrib.auth import logout as auth_logout
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.forms import (
+    AuthenticationForm, UserCreationForm, PasswordChangeForm)
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.views.decorators.http import require_http_methods, require_POST
+from django.contrib.auth.decorators import login_required
+
+from .forms import CustomUserChangeForm
 
 # Create your views here.
 @require_http_methods(['GET', 'POST'])
@@ -28,3 +31,54 @@ def logout(request):
         auth_logout(request)
     return redirect('articles:index')
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+        pass
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'accounts/signup.html', context)
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect('articles:index')
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(data=request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+def change_password(request):
+    if request.method == 'POST':
+        # PasswordChangeForm의 첫번째는 user를 넣어주세요
+        # form = PasswordChangeForm(user=request.user, data=request.POST)
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
